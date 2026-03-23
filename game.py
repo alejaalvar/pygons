a83078a573fb2ab6a49da5240eb8340cd6a8668d
Alejandro Alvarado <alejalva@pdx.edu>
2026-03-23 12:03:33 -0700

    implement fuzzy collision logic

--- game.py
+++ game.py
@@ -1,12 +1,22 @@
+from typing import List
 import pygame
 from constants import FPS, SCREEN_HEIGHT, SCREEN_WIDTH
-from space_object import Ship
+from space_object import SpaceObject, Ship, Asteroid
+
+
+def is_colliding(space_obj1: SpaceObject, space_obj2: SpaceObject) -> bool:
+    return (
+        space_obj1.position.distance_to(space_obj2.position)
+        < space_obj1.RADIUS + space_obj2.RADIUS
+    )
+
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pygons")
 clock = pygame.time.Clock()
 
+asteroids: List[Asteroid] = []
 ship = Ship(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 running = True
